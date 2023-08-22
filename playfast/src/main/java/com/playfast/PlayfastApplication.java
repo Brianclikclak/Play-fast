fb04dd676499b86404a4a8cb7378bd2a49f81aba
Brianclikclak <brianclikclak@hotmail.com>
2023-08-22 13:00:01 +0200

    creating project

---
+++ playfast/src/main/java/com/playfast/PlayfastApplication.java
@@ -0,0 +1,13 @@
+package com.playfast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayfastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayfastApplication.class, args);
+	}
+
+}
---
+++ playfast/src/test/java/com/playfast/PlayfastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playfast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayfastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
